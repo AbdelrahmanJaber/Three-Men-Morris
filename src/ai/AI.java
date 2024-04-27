1da68739e4a293a76c917630c28e9670132fbf80
AbdelrahmanJaber <abdelrahman.jaber999@gmail.com>
2024-04-27 16:22:30 +0300

    Implement Three Men Morris Game

---
+++ src/ai/AI.java
@@ -0,0 +1,28 @@
+
+package ai;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class AI {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+       
+//       public static void s(int m[][],int row,int column){
+//           m[row][column] = 1 ;
+ //      } 
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main_Page M_P = new Main_Page();
+        M_P.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        M_P.setLocationRelativeTo(null);
+        M_P.setVisible(true);
+   
+}
+}
